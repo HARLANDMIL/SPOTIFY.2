4d8214b1e13a1b0c1a9b428354acc02f8eda24a3
MILTON HARLAND <harlandmilton06@gmail.com>
2023-09-16 22:24:17 +0200

    ==UPDATED

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+//! CONNECTED FILES
+import { MainBox } from './Components/MainBox';
+import {LeftBox} from './Components/LeftBox';
+import {RightNavBar} from './Components/RightNavBar';
+
 function App() {
   return (
-    <div className="App">
-    <MainBox/>
-    
-      
+  <div className="App">
+  <MainBox/>;
+  <LeftBox/>
+  <RightNavBar/>
+
+//? FOR THE BACKGROUND
+  <div className="background"></div>
     </div>
     
   );
