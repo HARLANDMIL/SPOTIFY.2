3769fddf63c5e9474d97c0ee45caf354dc2d7604
MILTON HARLAND <harlandmilton06@gmail.com>
2023-09-19 21:59:56 +0200

    ==UPDATED

--- src/Components/LeftBox.js
+++ src/Components/LeftBox.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import '../Styles/LeftBox.css';
 import {FaSpotify,FaEquals} from "react-icons/fa"; //! Spotify logo
+import {HiSearch} from "react-icons/hi";
 
 
 function LeftBox() {
   return <div className='LeftMenu'>
-
-    <div id='logoImage'>
-    <i><FaSpotify /></i>
-    <h2>Spotify</h2>
-    <i><FaEquals/></i>
-      </div> //?ICONS
+      <div className='logoImage'>
+        <i><FaSpotify/></i>
+        <h2>Spotify</h2>
+        <i><FaEquals/></i>
+      </div>
     
-    <div className='searchBox'></div> //! Search Bar for searching songs
+    <div className='searchBox'>
+      <input type='text' placeholder='Search___'/>
+      <i>HiSearch</i>
+      </div> {/*Searching for songs*/}
   
   </div>
 
